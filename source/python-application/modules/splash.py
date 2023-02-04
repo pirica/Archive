78e8207b40351debe2590358b01ba8a791d169dc
Tectors <Tectors@users.noreply.github.com>
2023-02-04 13:29:11 -0700

    Update

--- source/python-application/modules/splash.py
+++ source/python-application/modules/splash.py
@@ -8,7 +8,7 @@ def get_splash(version, type="mobileBgImg"):
     # The endpoint
     endpoint = 'https://www.epicgames.com/fortnite/en-US/api/cms/home'
     # Request the api with the id and type
-    buffer = get(get(endpoint).json()['carousel']['slides'][0]['background']['desktopImage']).content
-
+    #buffer = get(get(endpoint).json()['carousel']['slides'][0]['background']['desktopImage']).content
+    buffer = get('https://cdn2.unrealengine.com/fortnite-overview-page-key-art-bg-alt-1920x1080-a6c439c942b6.jpg').content
     # Encode and return it
     return 'data:image/png;base64,' + str(base64.b64encode(buffer)).split('\'')[1].split('\'')[0]
