793389ff80caa080277fb0bbebdeb3e8f524f60c
Tectors <Tectors@users.noreply.github.com>
2023-02-04 13:18:03 -0700

    test?

--- source/python-application/modules/splash.py
+++ source/python-application/modules/splash.py
@@ -5,19 +5,8 @@ import base64
 
 # Gets the splash for the game
 def get_splash(version, type="mobileBgImg"):
-    v_end = get('https://www.epicgames.com/fortnite/en-US/api/cms/home').json()['pageClasses'].split('ch').pop().split('s')
-
     # The endpoint
     endpoint = 'https://www.epicgames.com/fortnite/en-US/api/cms/home'
-
-    # Parse the version into correct numbers (ex: 17 to 27)
-    version = [v_end[0], v_end[1]]
-
-    # The short-hand id
-    id = 'ch{0}s{1}'.format(version[0], str(int(version[1]) + 1))
-
-    print(id)
-
     # Request the api with the id and type
     buffer = get(get(endpoint).json()['carousel']['slides'][0]['background']['desktopImage']).content
 
